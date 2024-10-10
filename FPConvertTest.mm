630941bd3f676c0ab88d2df60d53564b7d5bde5d
Raghu Prasad <raghu@thinkpad.agileapes.com>
2024-10-10 15:59:33 +0530

    feat: Notebook to generate PDF file from mindmap
    - Added Jupyter notebook which reads and generates PDF file.
    - Added a sample mindmap created by Freeplane.
    - Created folder images/ to store sample images used in the sample mindmap.
    - Added an image containing ASCII art based wireframe of user-registration
      screen used in the sample mindmap.

---
+++ FPConvertTest.mm
@@ -0,0 +1,275 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Functional Specifications of Blooper" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1728222010462"><hook NAME="MapStyle" background="#f5f5dcff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sepia_template.mm" followedTemplateLocation="template:/light_sepia_template.mm" followedMapLastTime="1724074216000" fit_to_viewport="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-9 pt;" ENDINCLINATION="81.75 pt;19.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="User Registration" POSITION="bottom_or_right" ID="ID_86526980" CREATED="1727287685882" MODIFIED="1728222198294"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This module defines the user-registration aspect of the Blooper. Anonymous users are allowed to self-register and proceed to use the application.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="User Registration Form" POSITION="bottom_or_right" ID="ID_310393498" CREATED="1727283864138" MODIFIED="1728222425650"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Using a specified URL, anonymous users would be able to fetch a web-form to fill-up.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Form-Fields" ID="ID_861642195" CREATED="1728223219756" MODIFIED="1728232618809"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Following form-fields are presented to the user for self-registration.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Email Address" POSITION="bottom_or_right" ID="ID_1704485931" CREATED="1727283880479" MODIFIED="1728223490967"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      It would be unique across the user-database. That means, no two users can have same email address.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Required: Yes" ID="ID_7222657" CREATED="1728223557700" MODIFIED="1728463441381"/>
+<node TEXT="Max Size: char(128)" ID="ID_1247841701" CREATED="1728223253339" MODIFIED="1728232670877"/>
+<node TEXT="Allowed Values: .a-z0-9-_+@" ID="ID_877938460" CREATED="1728223285078" MODIFIED="1728232688902"/>
+<node TEXT="Unique: Yes" ID="ID_635656157" CREATED="1728463051601" MODIFIED="1728463057397"/>
+</node>
+<node TEXT="Mobile Number" POSITION="bottom_or_right" ID="ID_1429610052" CREATED="1727283888886" MODIFIED="1728237626000">
+<node TEXT="Required: Yes" ID="ID_1469988345" CREATED="1728223557700" MODIFIED="1728463422290"/>
+<node TEXT="Max Size: char(12)" ID="ID_1156785736" CREATED="1728223253339" MODIFIED="1728237665058"/>
+<node TEXT="Allowed Values: +-0123456789" ID="ID_707937760" CREATED="1728223285078" MODIFIED="1728237796496"/>
+<node TEXT="Unique: Yes" ID="ID_1488265487" CREATED="1728463051601" MODIFIED="1728463057397"/>
+</node>
+<node TEXT="Full Name" POSITION="bottom_or_right" ID="ID_1876960572" CREATED="1727283895955" MODIFIED="1728223163084">
+<node TEXT="Required: No" ID="ID_300463963" CREATED="1728223557700" MODIFIED="1728463427697"/>
+<node TEXT="Max Size: char(128)" ID="ID_640962968" CREATED="1728223253339" MODIFIED="1728463073797"/>
+<node TEXT="Allowed Values: A to Z, and a to z" ID="ID_1236504577" CREATED="1728223285078" MODIFIED="1728463099689"/>
+</node>
+<node TEXT="Address" POSITION="bottom_or_right" ID="ID_615179840" CREATED="1728223164574" MODIFIED="1728223175156">
+<node TEXT="Required: No" ID="ID_464726609" CREATED="1728223557700" MODIFIED="1728463433507"/>
+<node TEXT="Max Size: char(512)" ID="ID_1132425753" CREATED="1728223253339" MODIFIED="1728463147983"/>
+<node TEXT="Allowed Values: A-Z, a-z, 0-9, - +" ID="ID_213268044" CREATED="1728223285078" MODIFIED="1728463331516"/>
+</node>
+</node>
+<node TEXT="Buttons" ID="ID_303877642" CREATED="1728484729243" MODIFIED="1728484853081"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Following buttons are available on the screen.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Register" ID="ID_1572971016" CREATED="1728484737443" MODIFIED="1728484741806">
+<node TEXT="Normal Flow: Create new user-account, if possible." ID="ID_1542674567" CREATED="1728484759566" MODIFIED="1728484876096"/>
+<node TEXT="Exception Flow: Show validation errors, if any." ID="ID_69944687" CREATED="1728484877434" MODIFIED="1728484898261"/>
+</node>
+<node TEXT="Reset" ID="ID_1737270551" CREATED="1728484744929" MODIFIED="1728484746990">
+<node TEXT="Normal Flow: Clean-up contents of all fields." ID="ID_1022856176" CREATED="1728484908510" MODIFIED="1728484926971"/>
+</node>
+</node>
+<node TEXT="Links" ID="ID_1390704400" CREATED="1728484941031" MODIFIED="1728484962381"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Following hyperlinks are present on the screen.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Login Here" ID="ID_1580478443" CREATED="1728484963264" MODIFIED="1728485234450">
+<node TEXT="Normal Flow: If account is present, click to sign-in." ID="ID_1683460292" CREATED="1728484982735" MODIFIED="1728485250957"/>
+</node>
+</node>
+<node TEXT="Wireframes" ID="ID_777556651" CREATED="1728232476682" MODIFIED="1728486383431"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Following wireframe is proposed for the user-registration screen.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="User-registration Screen" ID="ID_571525042" CREATED="1728485097681" MODIFIED="1728485144207">
+<hook URI="images/user_register.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Level 1.2" POSITION="bottom_or_right" ID="ID_96957053" CREATED="1727283870685" MODIFIED="1728463124219">
+<node TEXT="Level 1.2.1" ID="ID_510466081" CREATED="1727283908987" MODIFIED="1727283914056">
+<node TEXT="Level 1.2.1.1" ID="ID_1292557226" CREATED="1727283928074" MODIFIED="1727283934859"/>
+<node TEXT="Level 1.2.1.2" ID="ID_564502373" CREATED="1727283935329" MODIFIED="1727288416338"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This is a note for Level 1.2.1.2.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Level 1.2.1.3" ID="ID_1966068569" CREATED="1727283940505" MODIFIED="1727283945052"/>
+<node TEXT="Level 1.2.1.4" ID="ID_487666157" CREATED="1727283945578" MODIFIED="1727283950585"/>
+</node>
+<node TEXT="Level 1.2.2" ID="ID_678241742" CREATED="1727283914534" MODIFIED="1728463127701"/>
+<node TEXT="Level 1.2.3" ID="ID_148633457" CREATED="1727283955346" MODIFIED="1727288403030"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This is a note for Level 1.2.3
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="Level 1.2.3.1" ID="ID_160398903" CREATED="1727283965046" MODIFIED="1727283970034"/>
+<node TEXT="Level 1.2.3.2" ID="ID_1123035921" CREATED="1727283970408" MODIFIED="1727284013651">
+<arrowlink DESTINATION="ID_160398903"/>
+</node>
+<node TEXT="Level 1.2.3.3" ID="ID_741612139" CREATED="1727283975921" MODIFIED="1727283980921"/>
+</node>
+</node>
+</node>
+<node TEXT="Level 2" POSITION="bottom_or_right" ID="ID_76613715" CREATED="1727287696924" MODIFIED="1727287700795">
+<node TEXT="Level 2.1" ID="ID_1020423966" CREATED="1727287734464" MODIFIED="1727287738476"/>
+<node TEXT="Level 2.2" ID="ID_1722806927" CREATED="1727287739014" MODIFIED="1727287742024">
+<node TEXT="Level 2.2.1" ID="ID_1376132053" CREATED="1727287742535" MODIFIED="1727287746603"/>
+<node TEXT="Level 2.2.2" ID="ID_684849006" CREATED="1727287747054" MODIFIED="1727287750531"/>
+<node TEXT="Level 2.2.3" ID="ID_1332164782" CREATED="1727287750969" MODIFIED="1727287754456">
+<node TEXT="Level 2.2.3.1" POSITION="bottom_or_right" ID="ID_892716213" CREATED="1727283986116" MODIFIED="1727290923703"/>
+</node>
+</node>
+</node>
+<node TEXT="Level 3" POSITION="top_or_left" ID="ID_1723722319" CREATED="1727287793032" MODIFIED="1727287796143"/>
+<node TEXT="Level 4" POSITION="top_or_left" ID="ID_547537959" CREATED="1727288086303" MODIFIED="1727288093864">
+<node TEXT="Level 4.1" ID="ID_1437007766" CREATED="1727288245124" MODIFIED="1727288250270"/>
+<node TEXT="Level 4.2" ID="ID_1215600471" CREATED="1727288250781" MODIFIED="1727288254260">
+<node TEXT="Level 4.2.1" ID="ID_1729614517" CREATED="1727288255566" MODIFIED="1727288260022"/>
+<node TEXT="Level 4.2.2" ID="ID_1420261931" CREATED="1727288261727" MODIFIED="1727288266042">
+<node TEXT="Level 4.2.2.1" ID="ID_266142024" CREATED="1727288267933" MODIFIED="1727288274631"/>
+<node TEXT="Level 4.2.2.2" ID="ID_1582207639" CREATED="1727288275012" MODIFIED="1727288281318"/>
+<node TEXT="Level 4.2.2.3" ID="ID_623684464" CREATED="1727288281909" MODIFIED="1727288298692">
+<arrowlink DESTINATION="ID_1723722319"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Level 5" POSITION="top_or_left" ID="ID_1395921346" CREATED="1727288094885" MODIFIED="1727288387286"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This is a note for Level 5.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</map>
